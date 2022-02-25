b2bebb72698b979776e53dc8502d48528318d859
div <div.pat499@gmail.com>
2022-02-24 22:15:57 -0500

    css design changes

--- mywebsite/src/components/Projects.js
+++ mywebsite/src/components/Projects.js
@@ -8,7 +8,7 @@ import { FaBootstrap, FaJava, FaNodeJs } from 'react-icons/fa';
 
 function Projects() {
     return (
-        <div className="flex flex-col from-gray-700 to-gray-900 bg-gradient-to-bl text-white rounded-xl pt-6 px-4 mt-12 shadow-2xl">
+        <div className="flex flex-col from-gray-700 to-gray-900 bg-gradient-to-bl text-white rounded-xl pt-6 px-4 mt-10 shadow-2xl">
 
             <div>
                 <p className='text-2xl sm:text-3xl font-semibold ml-10'>
@@ -24,6 +24,7 @@ function Projects() {
                     <Tags title="Project 1" icon={<SiAzuredevops className="inline-block text-lg mr-2 -mt-1" />} />
                     <Tags title="Project 1" icon={<SiTailwindcss className="inline-block text-lg mr-2 -mt-1" />} />
                     <Tags title="Project 1" icon={<FaBootstrap className="inline-block text-lg mr-2 -mt-1" />} />
+                    
                 </div>
             </div>
 
