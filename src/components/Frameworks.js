f72a3a03bd5a9deb3b15dabfbe7bfe3ffb665c16
div <div.pat499@gmail.com>
2022-03-09 20:23:38 -0500

    code cleanup

--- src/components/Frameworks.js
+++ src/components/Frameworks.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Tags from "./Tags";
 import {  SiExpress, SiMongodb, SiAzuredevops, SiGit, SiTailwindcss, SiAndroidstudio, SiDjango } from 'react-icons/si';
-import { FaBootstrap, FaJava, FaNodeJs } from 'react-icons/fa';
+import { FaBootstrap, FaNodeJs } from 'react-icons/fa';
 
 
 function Frameworks() {
@@ -11,10 +11,10 @@ function Frameworks() {
             className="flex flex-col items-center justify-between text-center 
                 p-6 from-gray-700 to-gray-900 bg-gradient-to-bl text-white 
                 shadow-2xl rounded-xl mb-8 mt-5 flex-1 md:mb-8  md:w-[45%] 
-                md:-ml-1 sm:flex-initial"       
+                md:-ml-1 sm:flex-initial" 
                 >
-
-            <h2 className='text-2xl sm:text-3xl font-semibold ml-10'>Frameworks/Tools</h2>
+        
+        <h2 className='text-2xl sm:text-3xl font-semibold ml-10'>Frameworks/Tools</h2>
 
         <div className='-mt-2 flex flex-wrap -m-1 p-6 sm:px-10'>
 
@@ -27,7 +27,6 @@ function Frameworks() {
         <Tags title="Android Studio" icon={<SiAndroidstudio className="inline-block text-lg mr-2 -mt-1" />} />
         <Tags title="Tailwind CSS" icon={<SiTailwindcss className="inline-block text-lg mr-2 -mt-1" />} />
         <Tags title="Bootstrap" icon={<FaBootstrap className="inline-block text-lg mr-2 -mt-1" />} />            
-        
         </div>
 
         </div>
