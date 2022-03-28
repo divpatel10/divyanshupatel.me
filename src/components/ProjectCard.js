90c32dc2f97dc08cf10e29d9fcf9241222541899
div <div.pat499@gmail.com>
2022-03-28 10:48:54 -0400

    Fixed Project Card CSS issue on mobile phones

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -14,30 +14,42 @@ function openWindow(url){
 
 function ProjectCard({img, title,  link}) {
     return (
-        <div 
-            class="flex-wrap inline-flex items-center
-                 justify-center mr-5 p-0.5 mb-2 overflow-hidden 
-                 text-md mt-5 font-medium text-violet-900
-                  rounded-lg group bg-gradient-to-br 
-                  from-violet-400 to-violet-800 
-                   md:w-[28%] dark:text-white"
-                   
-                   onClick={ e => {
-                    e.preventDefault();
-                    openWindow(link);
-                }
-                }
-
-                   >
-        <a href="#">
-            <img class="rounded-t-md object-fill h-28 w-48 border-4 border-black m-0 p-0" src={img} alt="" />
-        </a>
-        <div class="p-2">
-            <a href="#">
-                <h5 class="text-gray-900 font-bold text-md tracking-tight mb-2 dark:text-white">{title}</h5>
-            </a>
-        </div>
+  <div class="inline-block items-center justify-center min-w-[28%] px-4 py-2 ">
+    <div class="rounded-md overflow-hidden shadow-lg hover:scale-105 transition duration-500 cursor-pointer">
+      <div>
+        <img class="object-fill h-28 w-48 border-blue-500 border-2" src={img} alt="" />
+      </div>
+      <div class="py-2 px-2 bg-gradient-to-br from-blue-400 to-blue-600">
+        <h3 class="text-sm mb-2 font-semibold w-auto text-white">{title}</h3>
+      </div>
     </div>
+  </div>
+    //     <div 
+    //         class="flex-wrap inline-flex items-center
+    //              justify-center mr-5 p-0.5 mb-2 overflow-hidden 
+    //              text-md mt-5 font-medium text-violet-900
+    //               rounded-lg group bg-gradient-to-br 
+    //               from-violet-400 to-violet-800 
+    //                md:w-[28%] dark:text-white"
+                   
+    //                onClick={ e => {
+    //                 e.preventDefault();
+    //                 openWindow(link);
+    //             }
+    //             }
+
+    //                >
+
+
+    //     <a href="#">
+    //         <img class="rounded-t-md object-fill h-28 w-48 border-black m-0 p-0" src={img} alt="" />
+    //     </a>
+    //     <div class="p-2">
+    //         <a href="#">
+    //             <h5 class="text-gray-900 font-bold text-md tracking-tight mb-2 dark:text-white">{title}</h5>
+    //         </a>
+    //     </div>
+    // </div>
     );
 }
 
