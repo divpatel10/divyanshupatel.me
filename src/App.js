2e8fa0c840da79721806f619454171b2bd91bc2f
div <div.pat499@gmail.com>
2022-03-30 20:57:46 -0400

    removed unused Footer module import in App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Projects from "./components/Projects"
 import Skills from "./components/Skills"
 import AboutMe from "./components/AboutMe"
 import Frameworks from "./components/Frameworks"
-import Footer from './components/Footer';
 
 function App() {
   return (
